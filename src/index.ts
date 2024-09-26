46174361b4f1d703b70804a05c0cf7de3b931eea
Jamie Vangeysel <32323785+JamieVangeysel@users.noreply.github.com>
2024-09-26 14:31:53 +0200

    update deps, fix loglevel

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,13 @@ import { env } from 'process'
 
 import controller from './controller'
 
-const LOGLEVEL = 'debug'
+const LOGLEVEL = 'info'
 
 export default async function (config: any): Promise<FastifyInstance | undefined> {
   if (!config || !config.wrapper) return
-  if (!config.wrapper.mssql && config.mssql) config.wrapper.mssql = config.mssql
 
   const fastify = await Fastify({ ...config.wrapper })
+  fastify.log.level = LOGLEVEL
   await fastify.register(controller, { prefix: '/recipe', logLevel: LOGLEVEL })
   await fastify.listen({ port: +(env['PORT'] ?? 80), host: '::' })
 
